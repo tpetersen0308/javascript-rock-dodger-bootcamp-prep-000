bbf4e58a0f6e11e2a5953be3a3e7cfe72f4eef6f
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-29 20:32:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -136,11 +136,11 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   const code = e.which
-   if([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
-     e.preventDefault()
-     e.stopPropagation()
-   }
+  // const code = e.which
+   //if([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
+     //e.preventDefault()
+    // e.stopPropagation()
+  // }
 
    if(e.which === LEFT_ARROW) {
      moveDodgerLeft()
